1077cd40d9bc336b9f955f6d8a76404334ab83bf
Brandon Sturrock <brandon.sturrock@dynatrace.com>
2023-07-06 17:22:01 -0400

    version update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'dtsynthetic',
   packages = ['dtsynthetic'],
-  version = '0.2.15',
+  version = '0.2.16',
   description = 'Wrapper for the Dynatrace Synthetic API',
   author = 'Brandon Sturrock',
   author_email = 'brandon.sturrock@dynatrace.com',
